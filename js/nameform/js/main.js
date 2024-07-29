027a3d33375ffd6efffa3bd491030730a18c6b14
ehperminute <87844955+ehperminute@users.noreply.github.com>
2024-07-28 23:10:27 -0600

    Update main.js

--- js/nameform/js/main.js
+++ js/nameform/js/main.js
@@ -1,1 +1,11 @@
-
+submitElement = document.getElementById("submit");
+resultElement = document.querySelector(".result");
+username = document.getElementById("name");
+lastname = document.getElementById("lastname");
+submitElement.addEventListener("click", 
+    function() {
+        if (username.validity.valid && lastname.validity.valid){
+            resultElement.innerHTML = `Здравствуйте, ${username.validity.valid} ${lastname.value}`;
+            resultElement.style.textTransform = "capitalize";
+        }
+    });
